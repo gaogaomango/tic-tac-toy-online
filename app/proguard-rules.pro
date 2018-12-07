79a0ec7dfedd807558dfe2b50ca8619d870f7d02
Ogai Masataka <gaogaomango@gmail.com>
2018-12-08 03:43:24 +1100

    creaete login function with firebase

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,10 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+-dontwarn butterknife.internal.**
+-keep class **$$ViewInjector { *; }
+-keepnames class * { @butterknife.InjectView *;}
+
+-keepattributes Signature
+-keepattributes *Annotation*
--- app/src/main/java/jp/co/mo/tictactoyonline/MainActivity.java
+++ app/src/main/java/jp/co/mo/tictactoyonline/MainActivity.java
@@ -4,18 +4,37 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,25 +57,182 @@ public class MainActivity extends AppCompatActivity {
     private static final int BTN_ID_9 = BTN_ID_8 + +1;
 
     private int activePlayer = NO_PLAYER; // 1- for first, 2 for second
-    private List<Integer> player1; // hold player 1 data
-    private List<Integer> player2; // hold player 2 data
+    private List<Integer> mPlayer1; // hold player 1 data
+    private List<Integer> mPlayer2; // hold player 2 data
 
-    private Switch autoPlayStatusToggle;
-    private ProgressDialog progressDialog;
+    private ProgressDialog mProgressDialog;
+
+    @BindView(R.id.autoPlayStatusToggle)
+    Switch mAutoPlayStatusToggle;
+    @BindView(R.id.inviteUserNameText)
+    EditText mInviteUserNameText;
+    @BindView(R.id.inviteUserBtn)
+    Button mInviteUserBtn;
+    @BindView(R.id.acceptInvitationBtn)
+    Button mAcceptInvitationBtn;
+    @BindView(R.id.loginUserText)
+    EditText mLoginUserText;
+    @BindView(R.id.loginBtn)
+    Button mloginBtn;
+    @BindView(R.id.logoutBtn)
+    Button mLogoutBtn;
+    @BindView(R.id.registerBtn)
+    Button mRegisterBtn;
+
+    // Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initElement();
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    final String email = user.getEmail();
+                    if (!TextUtils.isEmpty(email)) {
+
+                        myRef.child("users")
+                                .child(beforeAt(email))
+                                .child("request")
+                                .setValue(user.getUid())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "onAuthState Changed: sign_in");
+                                        mLoginUserText.setText(email);
+                                        setElementStatus(true);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "onAuthState Changed: sign_in failed");
+                                        Log.d(TAG, e.getMessage());
+                                        mLoginUserText.setText("");
+                                        setElementStatus(false);
+                                    }
+                                });
+                    }
+                } else {
+                    Log.d(TAG, "onAuthState Changed: sign_out");
+                    mLoginUserText.setText("");
+                    setElementStatus(false);
+                }
+            }
+        };
+
     }
 
     private void initElement() {
         activePlayer = PLAYER_1_ID; // 1- for first, 2 for second
-        player1 = new ArrayList<>();
-        player2 = new ArrayList<>();
-        autoPlayStatusToggle = findViewById(R.id.autoPlayStatusToggle);
+        mPlayer1 = new ArrayList<>();
+        mPlayer2 = new ArrayList<>();
+    }
+
+    private void setElementStatus(boolean isLogin) {
+        mLoginUserText.setEnabled(!isLogin);
+        mRegisterBtn.setEnabled(!isLogin);
+        mRegisterBtn.setVisibility(isLogin ? View.GONE : View.VISIBLE);
+        mloginBtn.setEnabled(!isLogin);
+        mloginBtn.setVisibility(isLogin ? View.GONE : View.VISIBLE);
+        mLogoutBtn.setEnabled(isLogin);
+        mLogoutBtn.setVisibility(isLogin ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    private String beforeAt(String email) {
+        String[] str = email.split("@");
+        return str[0];
+    }
+
+    private void createUserAuthentiation(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signInUserAuthentication(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+    @OnClick(R.id.inviteUserBtn)
+    public void onClickInviteUserBtn(View view) {
+        Toast.makeText(this, "Click Invite UserBtn", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.acceptInvitationBtn)
+    public void onClickAcceptInvitationBtn(View view) {
+        Toast.makeText(this, "Click Accept InvitationBtn", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.registerBtn)
+    public void onClickRegisterBtn() {
+        createUserAuthentiation(mLoginUserText.getText().toString(), "gaogaomango");
+    }
+
+    @OnClick(R.id.loginBtn)
+    public void onClickLoginBtn(View view) {
+        signInUserAuthentication(mLoginUserText.getText().toString(), "gaogaomango");
+    }
+
+    @OnClick(R.id.logoutBtn)
+    public void onClickLogoutBtn(View view) {
+        signOut();
     }
 
     public void btnClick(View view) {
@@ -100,19 +276,19 @@ public class MainActivity extends AppCompatActivity {
         if (activePlayer == PLAYER_1_ID) {
             selectedBtn.setText("x");
             selectedBtn.setBackgroundColor(Color.GREEN);
-            player1.add(cellId);
+            mPlayer1.add(cellId);
             activePlayer = PLAYER_2_ID;
         } else if (activePlayer == PLAYER_2_ID) {
             selectedBtn.setText("0");
             selectedBtn.setBackgroundColor(Color.BLUE);
-            player2.add(cellId);
+            mPlayer2.add(cellId);
             activePlayer = PLAYER_1_ID;
         }
         selectedBtn.setEnabled(false);
         if (!isFinishedGame()) {
             // auto playをさせるのはplayer2だけ
             // auto play modeの場合はちょっとまってからプレイさせる
-            if (activePlayer == PLAYER_2_ID && autoPlayStatusToggle != null && autoPlayStatusToggle.isChecked()) {
+            if (activePlayer == PLAYER_2_ID && mAutoPlayStatusToggle != null && mAutoPlayStatusToggle.isChecked()) {
                 ProgressDialog progressDialog = new ProgressDialog(this);
                 progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                 progressDialog.setMessage("オートプレイを実行しています");
@@ -126,9 +302,9 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isFinishedGame() {
         int winner = NO_PLAYER;
-        winner = checkWinner(player1, PLAYER_1_ID);
+        winner = checkWinner(mPlayer1, PLAYER_1_ID);
         if (winner == NO_PLAYER) {
-            winner = checkWinner(player2, PLAYER_2_ID);
+            winner = checkWinner(mPlayer2, PLAYER_2_ID);
         }
 
         if (winner == NO_PLAYER) {
@@ -204,7 +380,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Find empty cells;
         for (int cellId = 1; cellId < 10; cellId++) {
-            if (!(player1.contains(cellId) || player2.contains(cellId))) {
+            if (!(mPlayer1.contains(cellId) || mPlayer2.contains(cellId))) {
                 emptyCells.add(cellId);
             }
         }
